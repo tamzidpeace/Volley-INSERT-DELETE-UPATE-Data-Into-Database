71164f09b9c687dcaa1b3ee7320d2bdde7935185
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-03-04 12:32:16 +0600

    Initial commit

---
+++ API/volley-send-data.php
@@ -0,0 +1,29 @@
+<?php 
+
+$user_name = $_POST["user_name"];
+$user_pass = $_POST["user_pass"];
+
+$username = "root";
+$password = "";
+$host = "localhost";
+$db_name = "user_db";
+$con = mysqli_connect($host, $username, $password, $db_name);
+
+
+
+$sql = "insert into registration_info values('".$user_name."', '".$user_pass."');";
+
+
+
+/*$sql = "INSERT INTO registration_info (user_name, user_pass)
+VALUES ('John', 'Doe')";*/
+
+if(mysqli_query($con, $sql)) {
+	echo "Data insertion successful";
+} else {
+	echo "Something went wrong";
+}
+
+?>
+
+ <!-- "http://192.168.43.30/volley-send-data.php" -->
---
+++ app/src/main/java/com/example/arafat/volley_save_data_into_database/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.arafat.volley_save_data_into_database;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    //const
+    private static final String TAG = "MainActivity";
+
+    //member variable
+    private EditText username;
+    private EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        Button save = findViewById(R.id.save);
+
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                RequestQueue requestQueue;
+
+
+                Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+                Network network = new BasicNetwork(new HurlStack());
+
+                requestQueue = new RequestQueue(cache, network);
+
+                requestQueue.start();
+
+                String url = "http://192.168.43.30/volley-send-data.php";
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Do something with the response
+                                Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // Handle error
+                                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+                                Log.d(TAG, "onErrorResponse: " + error.toString());
+                            }
+                        }) {
+                    @Override
+                    protected Map<String, String> getParams() {
+                        String name = username.getText().toString();
+                        String pass = password.getText().toString();
+
+                        Map<String, String> param = new HashMap<>();
+                        param.put("user_name", name);
+                        param.put("user_pass", pass);
+                        return param;
+                    }
+                };
+
+                MySingleton.getInstance(MainActivity.this).addToRequestQueue(stringRequest);
+
+            }
+        });
+    }
+}
